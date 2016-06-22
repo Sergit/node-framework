fcd04eb51679940b28e777e7d69160cf7188cf1b
Sergi <sergivalls@gmail.com>
2016-06-22 14:30:42 +0200

    handle selected node and delete.

--- Assets/Test/Editor/TestGraphAsset.cs
+++ Assets/Test/Editor/TestGraphAsset.cs
@@ -10,4 +10,24 @@ public class TestGraphAsset : TestGraph
 	{
 		return AssetInstanceCreator.Create(this);
 	}
+
+	public void DestroyNode(TestNode node)
+	{
+		if(node)
+		{
+			nodes.Remove(node);
+
+			foreach(ScriptableInput input in node.inputs)
+			{
+				DestroyImmediate(input,true);
+			}
+
+			foreach(ScriptableOutput output in node.outputs)
+			{
+				DestroyImmediate(output,true);
+			}
+
+			DestroyImmediate(node,true);
+		}
+	}
 }
--- Assets/Test/Editor/TestWindow.cs
+++ Assets/Test/Editor/TestWindow.cs
@@ -1,9 +1,13 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using System.Collections;
+using NodeFramework.Scriptable;
 
 public class TestWindow : EditorWindow
 {
+	[SerializeField]
+	TestNode m_SelectedNode;
+
 	[MenuItem ("Window/Test Window")]
 	static void Init()
 	{
@@ -54,19 +58,58 @@ public class TestWindow : EditorWindow
 
 			EndWindows();
 		}
+
+		HandleSelectedNode();
 	}
 
 	void DrawNode(int windowId)
 	{
 		TestNode node = m_Graph.nodes[windowId];
 
+		UpdateSelectedNode(node);
+
 		if(node)
 		{
 			Editor editor = Editor.CreateEditor(node);
-			editor.OnInspectorGUI();
-		}
 
-		GUI.DragWindow(new Rect(0, 0, 10000, 20));
+			editor.OnInspectorGUI();
+
+			GUI.DragWindow(new Rect(0, 0, 10000, 20));
+		}
+	}
+
+	void UpdateSelectedNode(TestNode node)
+	{
+		EventType type = Event.current.type;
+
+		if(type == EventType.MouseUp && Event.current.button == 0)
+		{
+			m_SelectedNode = node;
+		}
+	}
+
+	void HandleSelectedNode()
+	{
+		EventType type = Event.current.type;
+
+		if(m_SelectedNode)
+		{
+			switch(type) {
+			case EventType.KeyDown:
+
+				if(Event.current.keyCode == KeyCode.Delete || Event.current.keyCode == KeyCode.Backspace)
+				{
+					m_Graph.DestroyNode(m_SelectedNode);
+
+					Event.current.Use();
+				}
+
+				break;
+
+			default:
+				break;
+			}
+		}
 	}
 
 	void CreateNode()
